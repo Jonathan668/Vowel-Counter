ae33a997c4154ef82bcc544f68b83a7687230359
Jonathan668 <54424389+Jonathan668@users.noreply.github.com>
2021-03-15 01:39:50 -0400

    Update main.js

--- Vowel Counter/main.js
+++ Vowel Counter/main.js
@@ -1,11 +1,11 @@
 function countVowel() {
-    var str = document.getElementById("text").value;
-    var vowels = "aeiouAEIOU";
-    var count = 0;
+    let str = document.getElementById("text").value;
+    let vowels = "aeiouAEIOU";
+    let count = 0;
     
     // loop through string to test if each character is a vowel
-    for (var i = 0; i < str.length; i++) {
-        for (var j = 0; j < vowels.length; j++){
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < vowels.length; j++){
             if(str[i] == vowels[j]){
                 count++;
             }
